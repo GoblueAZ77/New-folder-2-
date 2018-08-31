365c907fb4e39725e9289fcefc614a25ee417db2
Goblueaz <Kdaly480@gmail.com>
2018-08-30 19:50:35 -0700

    initialcommit

---
+++ Models/PersonContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Tester.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext( ) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Repo/IPersonRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tester.Models;
+
+namespace Tester.Repo
+{
+    public class IPersonRepo
+    {
+        private PersonContext context = new PersonContext();
+        public IEnumerable<Person> People
+        {
+            get { return context.Persons; }
+        }
+
+    }
+}
